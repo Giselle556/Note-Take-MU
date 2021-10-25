facae4530d53a60b2f5839db55d2c8b321443fd0
Giselle556 <giselleborbolla@yahoo.com>
2021-10-25 13:22:47 -0700

    server.js

--- server.js
+++ server.js
@@ -1,3 +1,28 @@
-const expresss = require("express");
-const app = expresss();
+const express = require("express");
+const path = require("path");
+const apiRoutes = require('./routes/apiRoutes')
+
 const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// links to my assets
+app.use(express.static("public"));
+
+// page will load with index.html
+// html routes
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
+);
+
+// then with notes.html
+// html routes
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+app.use("/api", apiRoutes);
+
+app.listen(PORT, () =>
+console.log(`App listening at http://localhost:${PORT}`)
+);
