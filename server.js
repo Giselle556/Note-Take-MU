adea108306af507d0c2504b5cb23922eef7db515
Giselle556 <giselleborbolla@yahoo.com>
2021-11-02 22:28:33 -0700

    had to readd public file

--- server.js
+++ server.js
@@ -1,28 +1,77 @@
-const express = require("express");
-const path = require("path");
-const apiRoutes = require('./routes/apiRoutes')
-
 const PORT = process.env.PORT || 3001;
+const fs = require('fs');
+const path = require('path');
+
+const express = require('express');
 const app = express();
 
-app.use(express.json());
+const allNotes = require('./db/db.json');
+
 app.use(express.urlencoded({ extended: true }));
-// links to my assets
-app.use(express.static("public"));
+app.use(express.json());
+app.use(express.static('public'));
 
-// page will load with index.html
-// html routes
-app.get("/", (req, res) =>
-  res.sendFile(path.join(__dirname, "/public/index.html"))
-);
-
-// then with notes.html
-// html routes
-app.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/notes.html"));
+app.get('/api/notes', (req, res) => {
+    res.json(allNotes.slice(1));
 });
-app.use("/api", apiRoutes);
 
-app.listen(PORT, () =>
-console.log(`App listening at http://localhost:${PORT}`)
-);
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+function createNewNote(body, notesArray) {
+    const newNote = body;
+    if (!Array.isArray(notesArray))
+        notesArray = [];
+    
+    if (notesArray.length === 0)
+        notesArray.push(0);
+
+    body.id = notesArray[0];
+    notesArray[0]++;
+
+    notesArray.push(newNote);
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify(notesArray, null, 2)
+    );
+    return newNote;
+}
+
+app.post('/api/notes', (req, res) => {
+    const newNote = createNewNote(req.body, allNotes);
+    res.json(newNote);
+});
+
+function deleteNote(id, notesArray) {
+    for (let i = 0; i < notesArray.length; i++) {
+        let note = notesArray[i];
+
+        if (note.id == id) {
+            notesArray.splice(i, 1);
+            fs.writeFileSync(
+                path.join(__dirname, './db/db.json'),
+                JSON.stringify(notesArray, null, 2)
+            );
+
+            break;
+        }
+    }
+}
+
+app.delete('/api/notes/:id', (req, res) => {
+    deleteNote(req.params.id, allNotes);
+    res.json(true);
+});
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
