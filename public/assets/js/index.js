52d3b9ccc5d81bdfd23c9f215c6d22cf89c2c7ae
Giselle556 <giselleborbolla@yahoo.com>
2021-11-02 22:15:39 -0700

    added fun to delete notes

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -3,7 +3,7 @@ let noteText;
 let saveNoteBtn;
 let newNoteBtn;
 let noteList;
-//ask tutor about delete
+
 if (window.location.pathname === '/notes') {
   noteTitle = document.querySelector('.note-title');
   noteText = document.querySelector('.note-textarea');
@@ -12,17 +12,17 @@ if (window.location.pathname === '/notes') {
   noteList = document.querySelectorAll('.list-container .list-group');
 }
 
-
+// Show an element
 const show = (elem) => {
   elem.style.display = 'inline';
 };
 
-
+// Hide an element
 const hide = (elem) => {
   elem.style.display = 'none';
 };
 
-
+// activeNote is used to keep track of the note in the textarea
 let activeNote = {};
 
 const getNotes = () =>
@@ -77,9 +77,9 @@ const handleNoteSave = () => {
   });
 };
 
-//Need something to delete note/ FIX
+// Delete the clicked note
 const handleNoteDelete = (e) => {
- 
+  // prevents the click listener for the list from being called when the button inside of it is clicked
   e.stopPropagation();
 
   const note = e.target;
@@ -88,21 +88,21 @@ const handleNoteDelete = (e) => {
   if (activeNote.id === noteId) {
     activeNote = {};
   }
-//might need to fix this
+
   deleteNote(noteId).then(() => {
     getAndRenderNotes();
     renderActiveNote();
   });
 };
 
-
+// Sets the activeNote and displays it
 const handleNoteView = (e) => {
   e.preventDefault();
   activeNote = JSON.parse(e.target.parentElement.getAttribute('data-note'));
   renderActiveNote();
 };
 
-
+// Sets the activeNote to and empty object and allows the user to enter a new note
 const handleNewNoteView = (e) => {
   activeNote = {};
   renderActiveNote();
@@ -116,7 +116,7 @@ const handleRenderSaveBtn = () => {
   }
 };
 
-
+// Render the list of note titles
 const renderNoteList = async (notes) => {
   let jsonNotes = await notes.json();
   if (window.location.pathname === '/notes') {
@@ -125,13 +125,12 @@ const renderNoteList = async (notes) => {
 
   let noteListItems = [];
 
-
+  // Returns HTML element with or without a delete button
   const createLi = (text, delBtn = true) => {
     const liEl = document.createElement('li');
     liEl.classList.add('list-group-item');
 
     const spanEl = document.createElement('span');
-    spanEl.classList.add('list-item-title');
     spanEl.innerText = text;
     spanEl.addEventListener('click', handleNoteView);
 
@@ -170,7 +169,7 @@ const renderNoteList = async (notes) => {
   }
 };
 
-
+// Gets notes from the db and renders them to the sidebar
 const getAndRenderNotes = () => getNotes().then(renderNoteList);
 
 if (window.location.pathname === '/notes') {
